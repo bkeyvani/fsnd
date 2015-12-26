0d79c147c927e3a605148f33e4da8d0900b2bebc
Babak <webmaster@keyvani.com>
2015-12-26 15:12:05 -0500

    refactor: randomized PLAYERS list

--- p2/tournament-planner/populate.py
+++ p2/tournament-planner/populate.py
@@ -143,6 +143,9 @@ if __name__ == '__main__':
     # Register players
     PLAYERS = ['Player 1', 'Player 2', 'Player 3', 'Player 4', 'Player 5', 'Player 6', 'Player 7', 'Player 8', 'Player 9', 'Player 10', 'Player 11', 'Player 12', 'Player 13', 'Player 14', 'Player 15', 'Player 16', 'Player 17', 'Player 18', 'Player 19', 'Player 20', 'Player 21', 'Player 22', 'Player 23', 'Player 24', 'Player 25', 'Player 26', 'Player 27', 'Player 28', 'Player 29', 'Player 30', 'Player 31', 'Player 32', 'Player 33', 'Player 34', 'Player 35', 'Player 36', 'Player 37', 'Player 38', 'Player 39', 'Player 40', 'Player 41', 'Player 42', 'Player 43', 'Player 44', 'Player 45', 'Player 46', 'Player 47', 'Player 48', 'Player 49', 'Player 50', 'Player 51', 'Player 52', 'Player 53', 'Player 54', 'Player 55', 'Player 56', 'Player 57', 'Player 58', 'Player 59', 'Player 60', 'Player 61', 'Player 62', 'Player 63', 'Player 64',]
 
+    # Shuffle PLAYERS in order to have a random list
+    random.shuffle(PLAYERS)
+
     # Register all players
     for player in PLAYERS:
         registerPlayer(player)
